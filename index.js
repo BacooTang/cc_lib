d8454365017b2a68bbbfe88403fb1d7012ffdcb2
bacootang <bacootang@gmail.com>
2017-01-06 23:38:54 +0800

    增加获取网卡IP

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 'use strict';
 
+global.Promise = require('bluebird');
+
+global.get = Promise.promisify(require('request'));
+
+global.post = Promise.promisify(require('request').post);
+
 global.now = () =>{
 	return new Date().getTime();
 }
 
 global.time = () =>{
 	return Math.round(new Date().getTime()/1000);
+}
+
+global.get_ip_by_interface = (iface)=>{
+	const os = require('os');
+	const iface_info = os.networkInterfaces()[iface];
+	if(iface_info){
+		return iface_info[0][address];
+	}
 }
